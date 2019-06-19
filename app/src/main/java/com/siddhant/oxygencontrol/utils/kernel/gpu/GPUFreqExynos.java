54d030cbc99a94a146464030cdbdda5b5ba11b0d
DarkLord1731 <siddhantnaik17@gmail.com>
2019-06-19 20:58:01 +0530

    gpu: Add support for Exynos7885 GPU frequency control

--- app/src/main/java/com/siddhant/oxygencontrol/utils/kernel/gpu/GPUFreqExynos.java
+++ app/src/main/java/com/siddhant/oxygencontrol/utils/kernel/gpu/GPUFreqExynos.java
@@ -110,6 +110,17 @@ public class GPUFreqExynos {
     private static final String POWER_POLICY_EXYNOS7870 = "/sys/devices/11400000.mali/power_policy";
     private static final String UTILIZATION_EXYNOS7870 = "/sys/devices/11400000.mali/utilization";
 
+    private static final String MAX_EXYNOS7885_FREQ = "/sys/devices/platform/11500000.mali/max_clock";
+    private static final String MIN_EXYNOS7885_FREQ = "/sys/devices/platform/11500000.mali/min_clock";
+    private static final String CUR_EXYNOS7885_FREQ = "/sys/devices/platform/11500000.mali/clock";
+    private static final String AVAILABLE_EXYNOS7885_FREQS = "/sys/devices/platform/11500000.mali/volt_table";
+    private static final String AVAILABLE_EXYNOS7885_GOVERNORS = "/sys/devices/platform/11500000.mali/dvfs_governor";
+    private static final String TUNABLE_HIGHSPEED_EXYNOS7885_CLOCK = "/sys/devices/platform/11500000.mali/highspeed_clock";
+    private static final String TUNABLE_HIGHSPEED_EXYNOS7885_LOAD = "/sys/devices/platform/11500000.mali/highspeed_load";
+    private static final String TUNABLE_HIGHSPEED_EXYNOS7885_DELAY = "/sys/devices/platform/11500000.mali/highspeed_delay";
+    private static final String POWER_POLICY_EXYNOS7885 = "/sys/devices/platform/11500000.mali/power_policy";
+    private static final String UTILIZATION_EXYNOS7885 = "/sys/devices/platform/11500000.mali/utilization";
+
     private final HashMap<String, Integer> mAvailableVolts = new HashMap<>();
     private final HashMap<String, Integer> mCurrentFreqs = new HashMap<>();
     private final List<String> mMaxFreqs = new ArrayList<>();
@@ -128,18 +139,21 @@ public class GPUFreqExynos {
         mAvailableVolts.put(AVAILABLE_S9_FREQS, 1000);
         mAvailableVolts.put(AVAILABLE_S10_FREQS, 1000);
         mAvailableVolts.put(AVAILABLE_EXYNOS7870_FREQS, 1000);
+        mAvailableVolts.put(AVAILABLE_EXYNOS7885_FREQS, 1000);
 
         mCurrentFreqs.put(CUR_S7_FREQ, 1);
         mCurrentFreqs.put(CUR_S8_FREQ, 1);
         mCurrentFreqs.put(CUR_S9_FREQ, 1);
         mCurrentFreqs.put(CUR_S10_FREQ, 1);
         mCurrentFreqs.put(CUR_EXYNOS7870_FREQ, 1);
+        mCurrentFreqs.put(CUR_EXYNOS7885_FREQ, 1);
 
         mMaxFreqs.add(MAX_S7_FREQ);
         mMaxFreqs.add(MAX_S8_FREQ);
         mMaxFreqs.add(MAX_S9_FREQ);
         mMaxFreqs.add(MAX_S10_FREQ);
         mMaxFreqs.add(MAX_EXYNOS7870_FREQ);
+        mMaxFreqs.add(MAX_EXYNOS7885_FREQ);
         mMaxFreqs.add(MAX_S7_FREQ_STOCK);
         mMaxFreqs.add(MAX_FREQ_STOCK);
 
@@ -148,6 +162,7 @@ public class GPUFreqExynos {
         mMinFreqs.add(MIN_S9_FREQ);
         mMinFreqs.add(MIN_S10_FREQ);
         mMinFreqs.add(MIN_EXYNOS7870_FREQ);
+        mMinFreqs.add(MIN_EXYNOS7885_FREQ);
         mMinFreqs.add(MIN_S7_FREQ_STOCK);
         mMinFreqs.add(MIN_FREQ_STOCK);
 
@@ -156,6 +171,7 @@ public class GPUFreqExynos {
         mAvailableFreqs.add(AVAILABLE_S9_FREQS);
         mAvailableFreqs.add(AVAILABLE_S10_FREQS);
         mAvailableFreqs.add(AVAILABLE_EXYNOS7870_FREQS);
+        mAvailableFreqs.add(AVAILABLE_EXYNOS7885_FREQS);
         mAvailableFreqs.add(AVAILABLE_S7_FREQS_STOCK);
         mAvailableFreqs.add(AVAILABLE_FREQS_STOCK);
 
@@ -164,36 +180,42 @@ public class GPUFreqExynos {
         mScalingGovernors.add(AVAILABLE_S9_GOVERNORS);
         mScalingGovernors.add(AVAILABLE_S10_GOVERNORS);
         mScalingGovernors.add(AVAILABLE_EXYNOS7870_GOVERNORS);
+        mScalingGovernors.add(AVAILABLE_EXYNOS7885_GOVERNORS);
 
         mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_S7_CLOCK, 1);
         mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_S8_CLOCK, 1);
         mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_S9_CLOCK, 1);
         mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_S10_CLOCK, 1);
         mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_EXYNOS7870_CLOCK, 1);
+        mTunableHighspeedClocks.put(TUNABLE_HIGHSPEED_EXYNOS7885_CLOCK, 1);
 
         mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_S7_LOAD, 1);
         mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_S8_LOAD, 1);
         mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_S9_LOAD, 1);
         mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_S10_LOAD, 1);
         mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_EXYNOS7870_LOAD, 1);
+        mTunableHighspeedLoads.put(TUNABLE_HIGHSPEED_EXYNOS7885_LOAD, 1);
 
         mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_S7_DELAY, 1);
         mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_S8_DELAY, 1);
         mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_S9_DELAY, 1);
         mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_S10_DELAY, 1);
         mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_EXYNOS7870_DELAY, 1);
+        mTunableHighspeedDelays.put(TUNABLE_HIGHSPEED_EXYNOS7885_DELAY, 1);
 
         mPowerPolicies.put(POWER_POLICY_S7, 1);
         mPowerPolicies.put(POWER_POLICY_S8, 1);
         mPowerPolicies.put(POWER_POLICY_S9, 1);
         mPowerPolicies.put(POWER_POLICY_S10, 1);
         mPowerPolicies.put(POWER_POLICY_EXYNOS7870, 1);
+        mPowerPolicies.put(POWER_POLICY_EXYNOS7885, 1);
 
         mUtilization.put(UTILIZATION_S7, 1);
         mUtilization.put(UTILIZATION_S8, 1);
         mUtilization.put(UTILIZATION_S9, 1);
         mUtilization.put(UTILIZATION_S10, 1);
         mUtilization.put(UTILIZATION_EXYNOS7870, 1);
+        mUtilization.put(UTILIZATION_EXYNOS7885, 1);
     }
 
     public String AVAILABLE_VOLTS;
